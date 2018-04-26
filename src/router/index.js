d4de1eacdc3d8c753700910f8b7ddd67b9be74f9
wyx <469699659@qq.com>
2018-04-26 14:41:09 +0800

    wyx 航班动态

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import Main from './../components/main/main'
 import kaxw_list from "@/components/kaxw/kaxw_list.vue"
 import kaxw_details from "@/components/kaxw/kaxw_details.vue"
 import search from "@/components/kaxw/search.vue"
+import Flight from "@/components/Flight/flightMain"
+import Destination from "@/components/Flight/destination"
+import MyList from "@/components/Flight/myList"
+import SelectPalne from "@/components/Flight/selectPlane"
+
 
 Vue.use(Router)
 
@@ -14,11 +19,6 @@ export default new Router({
 			path: '/',
 			name: 'home',
 			component: Home
-		},
-		{
-			path: '/main',
-			name: 'main',
-			component: Main
 		},
 		//口岸新闻列表
 		{
@@ -37,6 +37,28 @@ export default new Router({
 			path: '/kaxw/search',
 			name: 'search',
 			component: search
-		}
+		},
+    //航班动态
+    {
+      path:'/flight',
+      name:'Flight',
+      component:Flight,
+      redirect: '/flight/destination',
+      children:[{
+        path: '/flight/destination',
+        name: 'Destination',
+        component: Destination,
+      },{
+        path: '/flight/myList',
+        name: 'MyList',
+        component: MyList
+      }
+      ]
+    },
+    {
+      path: '/flight/selectpalne',
+      name: 'SelectPalne',
+      component: SelectPalne,
+    }
 	]
 })
